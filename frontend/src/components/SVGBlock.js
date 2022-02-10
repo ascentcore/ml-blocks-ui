18ee50636b9c3437ca32dc786ab91c740e4b67de
ardeleanana <61140469+ardeleanana@users.noreply.github.com>
2022-02-10 14:48:21 +0200

    #7 Rename r to radius

--- frontend/src/components/SVGBlock.js
+++ frontend/src/components/SVGBlock.js
@@ -49,13 +49,13 @@ function SVGBlock({ block, transform }) {
         fetchData()
     }, [timer])
 
-    const r = 6;
+    const radius = 6;
     function getUpstream(upstreamBlock) {
         const [ux, uy] = upstreamBlock.location
         const [dx, dy] = block.location
         let str = `
-            M ${ux + blockWidth + r} ${uy + offset}
-              ${dx - r} ${dy + offset}`
+            M ${ux + blockWidth + radius} ${uy + offset}
+              ${dx - radius} ${dy + offset}`
 
         return (<path d={str} stroke="#000" fill="transparent" strokeWidth={1} />)
     }
@@ -67,15 +67,15 @@ function SVGBlock({ block, transform }) {
                     <g transform={`translate(${block.location[0]}, ${block.location[1]})`}>
                         <text textAnchor='middle' x={blockWidth / 2}>{status.name}</text>
                         <rect stroke="#000" x={0} y={10} strokeWidth="1" width={blockWidth} height={blockHeight} fill="transparent" />
-                        {status.status.map((state, index) => (
+                        {status.status.map(state => (
                             <>
-                                <circle cx="0" cy="30" r={r} className={(state.state_name === states[1] || state.state_name === states[1]) ? classes.fill : classes.circle} />
+                                <circle cx="0" cy="30" r={radius} className={(state.state_name === states[1] || state.state_name === states[1]) ? classes.fill : classes.circle} />
                                 <path d="M 6 30 H 100 " className={(state.state_name === states[2]) ? classes.stroke : classes.path} />
                                 <circle cx="100" cy="30" r="8" className={state.state_name === states[2] ? classes.fill : classes.circle} />
                                 <path d="M 200 30 H 108 " className={classes.path} />
-                                <circle cx="200" cy="30" r={r} className={state.state_name === states[4] ? classes.fill : classes.circle} />
-                                <path d="M 100 38 C 100 72, 90 65, 200 65" className={state.state_name === state[3] ? classes.stroke : classes.circle} />
-                                <circle cx="200" cy="65" r={r} className={state.state_name === states[4] ? classes.fill : classes.circle} />
+                                <circle cx="200" cy="30" r={radius} className={state.state_name === states[4] ? classes.fill : classes.circle} />
+                                <path d="M 100 38 C 100 72, 90 65, 200 65" className={state.state_name === state[3] ? classes.stroke : classes.path} />
+                                <circle cx="200" cy="65" r={radius} className={state.state_name === states[4] ? classes.fill : classes.circle} />
                             </>
                         ))}
                     </g>
