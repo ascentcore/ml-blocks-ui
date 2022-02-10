8c05b4b1a33e5e97b01c07e795bff9a0374ab891
ardeleanana <61140469+ardeleanana@users.noreply.github.com>
2022-02-10 14:23:22 +0200

    #7 Style based on status

--- frontend/src/components/SVGBlock.js
+++ frontend/src/components/SVGBlock.js
@@ -1,8 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import { getStatusOfIp } from '../api/data';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(() => ({
+    '@keyframes dash': {
+        to: {
+            strokeDashoffset: -70,
+        }
+    },
+    path: {
+        stroke: '#000',
+        fill: 'transparent'
+    },
+    stroke: {
+        strokeDasharray: 5,
+        stroke: '#000',
+        fill: 'transparent',
+        animation: `$dash linear infinite`,
+        animationDuration: '5s'
+
+    },
+    circle: {
+        stroke: "#000",
+        fill: '#fff'
+    },
+    fill: {
+        stroke: "#000",
+        fill: 'green'
+    }
+}))
 
 function SVGBlock({ block, transform }) {
-
+    const states = ['statics', 'ingesting', 'processing', 'storing', 'pending'];
+    const classes = useStyles();
     const [status, setStatus] = useState()
     const [timer, setTimer] = useState(Date.now())
 
@@ -19,40 +49,40 @@ function SVGBlock({ block, transform }) {
         fetchData()
     }, [timer])
 
+    const r = 6;
     function getUpstream(upstreamBlock) {
         const [ux, uy] = upstreamBlock.location
         const [dx, dy] = block.location
         let str = `
-            M ${ux + blockWidth} ${uy + offset}
-              ${dx} ${dy + offset}`
+            M ${ux + blockWidth + r} ${uy + offset}
+              ${dx - r} ${dy + offset}`
 
         return (<path d={str} stroke="#000" fill="transparent" strokeWidth={1} />)
     }
 
     return (
-        <>
-            <g transform={transform}>
-                {status && (
-                    <g>
-                        <g transform={`translate(${block.location[0]}, ${block.location[1]})`}>
-                            <text textAnchor='middle' x={blockWidth / 2}>{status.name}</text>
-                            <rect stroke="#000" x={0} y={10} strokeWidth="1" width={blockWidth} height={blockHeight} fill="transparent" />
-                            {status?.status[0].state_name === 'pending'
-                                ? <circle cx="0" cy="30" r="6" stroke="#000" fill='green' />
-                                : <circle cx="0" cy="30" r="6" stroke="#000" fill='#fff' />
-                            }
-                            <path d="M 6 30 H 100 " fill="transparent" stroke="#000" />
-                            <circle cx="100" cy="30" r="8" stroke="#000" fill='#fff' />
-                            <path d="M 200 30 H 108 " fill="transparent" stroke="#000" />
-                            <circle cx="200" cy="30" r="6" stroke="#000" fill='#fff' />
-                            <path d="M 100 38 C 104 75, 90 65, 200 65" stroke="#000" fill="transparent" />
-                            <circle cx="200" cy="65" r="6" stroke="#000" fill='#fff' />
-                        </g>
-                        {Object.values(block.upstream).map(upstreamBlock => getUpstream(upstreamBlock))}
+        <g transform={transform}>
+            {status && (
+                <g>
+                    <g transform={`translate(${block.location[0]}, ${block.location[1]})`}>
+                        <text textAnchor='middle' x={blockWidth / 2}>{status.name}</text>
+                        <rect stroke="#000" x={0} y={10} strokeWidth="1" width={blockWidth} height={blockHeight} fill="transparent" />
+                        {status.status.map((state, index) => (
+                            <>
+                                <circle cx="0" cy="30" r={r} className={(state.state_name === states[1] || state.state_name === states[1]) ? classes.fill : classes.circle} />
+                                <path d="M 6 30 H 100 " className={(state.state_name === states[2]) ? classes.stroke : classes.path} />
+                                <circle cx="100" cy="30" r="8" className={state.state_name === states[2] ? classes.fill : classes.circle} />
+                                <path d="M 200 30 H 108 " className={classes.path} />
+                                <circle cx="200" cy="30" r={r} className={state.state_name === states[4] ? classes.fill : classes.circle} />
+                                <path d="M 100 38 C 100 72, 90 65, 200 65" className={state.state_name === state[3] ? classes.stroke : classes.circle} />
+                                <circle cx="200" cy="65" r={r} className={state.state_name === states[4] ? classes.fill : classes.circle} />
+                            </>
+                        ))}
                     </g>
-                )}
-            </g>
-        </>
+                    {Object.values(block.upstream).map(upstreamBlock => getUpstream(upstreamBlock))}
+                </g>
+            )}
+        </g>
     )
 };
 
