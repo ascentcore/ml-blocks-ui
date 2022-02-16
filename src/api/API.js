49e9ec5d1cdd42273dd27a8237360986743eb08e
Cornel Stefanache <cornel.stefanache@gmail.com>
2022-02-16 11:54:17 +0200

    added env var

--- src/api/API.js
+++ src/api/API.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const API_BASE = `http://${process.env.REGISTRY || 'localhost:9080'}/api/v1`;
+console.log('#####--', process.env)
+export const API_BASE = `http://${process.env.REACT_APP_REGISTRY || 'localhost:9080'}/api/v1`;
 
 export default axios.create({
     baseURL: API_BASE,
