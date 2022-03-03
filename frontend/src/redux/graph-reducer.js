cf625ec26044e0f6912158a9a7dd1053e37e314b
ardeleanana <61140469+ardeleanana@users.noreply.github.com>
2022-03-03 13:25:26 +0200

    #26 Add ip and graph reducers

---
+++ frontend/src/redux/graph-reducer.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialStateValue = '';
+
+export const graphSlice = createSlice({
+    name: 'graph',
+    initialState: { value: initialStateValue },
+    reducers: {
+        getGraphReducer: (state, action) => {
+            state.value = action.payload
+        },
+    },
+})
+
+export const { getGraphReducer } = graphSlice.actions;
+
+export default graphSlice.reducer;
