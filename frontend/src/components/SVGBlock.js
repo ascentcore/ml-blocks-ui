3b653c928cbc6f5dc6a3e4184ae9fd0e6fbb248f
Cornel Stefanache <cornel.stefanache@gmail.com>
2022-03-05 17:37:46 +0200

    bettter graph calculation

--- frontend/src/components/SVGBlock.js
+++ frontend/src/components/SVGBlock.js
@@ -34,12 +34,17 @@ const useStyles = makeStyles(() => ({
         animation: `$dash linear infinite`,
         animationDuration: '5s'
     },
+    dot_stroke: {
+        strokeDasharray: 5,
+        stroke: '#000',
+        fill: 'transparent',
+    },
     animated_circle: {
         fill: '#8ad18c',
         stroke: '#000',
         strokeWidth: 1,
         animation: `$loading linear infinite`,
-        animationDuration: '3s'
+        animationDuration: '2s'
 
     },
     pending_circle: {
@@ -64,12 +69,11 @@ const useStyles = makeStyles(() => ({
 }))
 
 function SVGBlock({ block, transform, selected, onClick }) {
-    const states = ['ingesting', 'processing', 'storing', 'statics', 'training', 'pending'];
+    const states = ['ingesting', 'processing', 'storing', 'statics', 'training', 'predicting', 'pending'];
     const classes = useStyles();
     const [status, setStatus] = useState();
     const [state, setState] = useState();
     const [timer, setTimer] = useState(Date.now());
-    const [fetching, setFetching] = useState(false)
 
     const radius = 6;
     const blockWidth = 200
@@ -77,21 +81,23 @@ function SVGBlock({ block, transform, selected, onClick }) {
     const offset = 30
 
     useEffect(() => {
+        fetching = false
         async function fetchData() {
             if (fetching === false) {
-                setFetching(true)
+                fetching = true
                 const response = await getStatusOfIp(block.ip)
-                if (response.data.status && response.data.status.length) {
-                    setState(response.data.status[0])
+                if (response.data.status) {
+                    setState(response.data.status)
                     setStatus(response.data)
+                    fetching = false
                 }
-                setFetching(false)
+                
             }
         }
         fetchData()
-        // const interval = setInterval(() => {
-        //     fetchData()
-        // }, 5000)
+        const interval = setInterval(() => {
+            fetchData()
+        }, 5000)
         return () => clearInterval(interval)
 
     }, [timer])
@@ -100,16 +106,27 @@ function SVGBlock({ block, transform, selected, onClick }) {
         return state && state.state_name === states[index]
     }
 
-    function getUpstream(upstreamBlock) {
-        const [ux, uy] = upstreamBlock.location
-        const [dx, dy] = block.location
+    function getUpstream(upstreamEdge) {
+        const { block: upstreamBlock, edgeType } = upstreamEdge
+        let [ux, uy] = upstreamBlock.location
+        let [dx, dy] = block.location
+        if (edgeType === 1) {
+            uy += 35
+            dy += 35
+        }
+
+        let [sx, sy, ex, ey] = [ux + blockWidth + radius, uy + offset, dx - radius, dy + offset]
+
         let str = `
-            M ${ux + blockWidth + radius} ${uy + offset}
-              ${dx - radius} ${dy + offset}`
+            M ${sx} ${sy}
+            C ${sx + 70} ${sy}, ${ex - 70} ${ey}
+              ${ex} ${ey}`
+
+        let className = edgeType === 1 ? classes.dot_stroke : isState(0) ? classes.animated_stroke : classes.path
         return (<path
             key={upstreamBlock.ip}
             d={str}
-            className={isState(0) ? classes.animated_stroke : classes.path} fill="transparent" strokeWidth={1} />)
+            className={className} fill="transparent" strokeWidth={1} />)
     }
 
     function getCircleState(circleState) {
@@ -124,6 +141,7 @@ function SVGBlock({ block, transform, selected, onClick }) {
                     <text textAnchor='middle' x={blockWidth / 2}>{status ? status.name : 'Status Pending...'}</text>
                     <rect x={0} y={10} width={blockWidth} height={blockHeight} className={selected ? classes.selectedRect : classes.rect} onClick={onClick} />
                     <circle cx="0" cy="30" r={radius} className={getCircleState(0)} />
+                    <circle cx="0" cy="65" r={radius} className={classes.pending_circle} />
 
                     <path d="M 6 30 H 100 " className={isState(1) ? classes.animated_stroke : classes.path} markerEnd="url(#arrow-1)" />
                     <circle cx="100" cy="30" r="8" className={getCircleState(1)} />
@@ -131,8 +149,8 @@ function SVGBlock({ block, transform, selected, onClick }) {
                     <path d="M 200 30 H 108 " className={classes.path} markerEnd="url(#arrow-2)" />
                     <circle cx="200" cy="30" r={radius} className={getCircleState(3)} />
 
-                    <path d="M 100 38 C 100 72, 90 65, 200 65" className={isState(4) ? classes.animated_stroke : classes.path} markerEnd="url(#arrow-3)" />
-                    <circle cx="200" cy="65" r={radius} className={getCircleState(4)} />
+                    <path d="M 100 38 C 100 72, 90 65, 200 65" className={isState(5) ? classes.animated_stroke : classes.path} markerEnd="url(#arrow-3)" />
+                    <circle cx="200" cy="65" r={radius} className={getCircleState(5)} />
                 </g>
                 {Object.values(block.upstream).map(upstreamBlock => getUpstream(upstreamBlock))}
             </g>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -12,7 +12,6 @@ const HomeScreen = () => {
     useEffect(() => {
         async function fetchData() {
             const response = await getGraph()
-            const localBlocks = []
             const blocks = {}
 
             const registerBlock = (ip) => {
@@ -21,55 +20,49 @@ const HomeScreen = () => {
                 if (ip !== 'None' && !blocks[ip]) {
                     block = {
                         ip,
-                        location: [0, 20],
-                        downstream: {},
-                        upstream: {}
+                        location: [0, 0],
+                        upstream: [],
+                        downstream: []
                     }
 
                     blocks[ip] = block
 
-                    localBlocks.push(block)
                 }
 
                 return block
             }
 
-            response.data.forEach(({ upstream, downstream }) => {
+            response.data.forEach(({ upstream, downstream, edge_type }) => {
                 const upstreamBlock = registerBlock(upstream)
-                const downstreamBlock = registerBlock(downstream, true)
+                const downstreamBlock = registerBlock(downstream)
                 if (upstreamBlock && downstreamBlock) {
-                    upstreamBlock.downstream[downstreamBlock.ip] = downstreamBlock
-                    downstreamBlock.upstream[upstreamBlock.ip] = upstreamBlock
-                }
-            });
-
-            function repositionDownstream(currentBlock) {
-                let lastYVal = currentBlock.location[1]
-                Object.values(currentBlock.downstream).forEach((block, index) => {
-                    if (!block.hasPosition) {
-                        const [cx, cy] = currentBlock.location
-                        block.location = [cx + 300, cy + 140 * index]
-                        block.hasPosition = true
-                        lastYVal = Math.max(lastYVal, repositionDownstream(block))
-                    }
-                })
-
-                return lastYVal
-            }
-
-            let lastYVal = 30
-            localBlocks.forEach(block => {
-                if (Object.keys(block.upstream).length === 0) {
-                    block.location[1] = lastYVal
-                    lastYVal = repositionDownstream(block)
+                    downstreamBlock.upstream.push({ block: upstreamBlock, edgeType: edge_type })
+                    upstreamBlock.downstream.push({ block: downstreamBlock, edgeType: edge_type })
                 }
             })
 
-            setBlocks(localBlocks)
+            let currentY = 0
 
-            console.log(localBlocks)
+            const roots = Object.values(blocks).filter(item => item.upstream.length === 0)
+            const positionBlock = (x, block, currentY) => {
+                let startFromY = currentY
+                if (!block.visited) {
+                    block.location = [x, startFromY]
+                    block.downstream.forEach(downstreamBlock => {
+                        positionBlock(x + 300, downstreamBlock.block, startFromY)
+                        startFromY += 120
+                    })
+                    block.visited = true
+                }
+                return startFromY
+            }
 
-            return setGraph(response.data)
+            roots.forEach(root => {
+                currentY = positionBlock(0, root, currentY)
+            })
+
+
+            setBlocks(Object.values(blocks))
         }
         fetchData();
     }, [])
