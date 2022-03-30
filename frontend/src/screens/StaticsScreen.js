104868c766a01b1877b070220bc86e9f349ca76d
Cornel Stefanache <cornel.stefanache@gmail.com>
2022-03-30 23:04:57 +0300

    updated statics screen

--- frontend/src/screens/StaticsScreen.js
+++ frontend/src/screens/StaticsScreen.js
@@ -1,109 +1,110 @@
-import React, { useEffect, useState } from 'react';
-import { getTargetIP } from '../api/API';
-import * as getIP from '../api/data';
-import { download } from '../api/data';
-import { makeStyles } from '@mui/styles';
-import { Grid, List, ListItem, ListItemText, TextField, Typography } from '@mui/material';
-import DownloadIcon from '@mui/icons-material/Download';
-import { Link } from 'react-router-dom';
-import { API_BASE } from '../api/API';
-import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import React, { useEffect, useState } from "react";
+import { getTargetIP } from "../api/API";
+import * as getIP from "../api/data";
+import { makeStyles } from "@mui/styles";
+import { Grid, TextField } from "@mui/material";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import moment from "moment";
+import { Link } from "react-router-dom";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
 
 export const useStyles = makeStyles((theme) => ({
-    list: {
-        width: '300px',
-        marginTop: '100px'
-    },
-    image: {
-        maxWidth: '100%',
-        height: 'auto'
-    },
-    search: {
-        marginBottom: '30px',
-        width: '300px',
-    }
-}))
+  list: {
+    width: "300px",
+    marginTop: "100px",
+  },
+  image: {
+    // maxWidth: "100%",
+    // height: "auto",
+    height: 50,
+  },
+  search: {
+    marginBottom: "30px",
+    width: "300px",
+  },
+}));
 
 const StaticsScreen = () => {
-    const classes = useStyles();
-    const [value, setValue] = useState();
-    const [image, setImage] = useState('');
-    const [filteredItems, setFilteredItems] = useState();
+  const classes = useStyles();
+  const [filteredItems, setFilteredItems] = useState();
 
-    let ip = getTargetIP()
+  let ip = getTargetIP();
 
-    useEffect(() => {
-        if (ip === undefined && path === '') return
-        async function fetchData() {
-            const response = await getIP.getProxy(ip, 'api/v1/statics/')
-            setFilteredItems(response.data)
-            return setValue(response.data)
-        }
-
-        fetchData();
-    }, [ip]);
-
-    const getIconForFile = item => {
-        const re = /(?:\.([^.]+))?$/;
-        const extension = re.exec(item)[1]
-        switch (extension) {
-            case 'png':
-            case 'jpg':
-            case 'jpeg':
-                return <img className={classes.image} src={`/proxy/${ip}/api/v1/download/${item}`} />
-
-            default:
-                return <InsertDriveFileIcon />
-        }
+  useEffect(() => {
+    if (ip === undefined && path === "") return;
+    async function fetchData() {
+      const response = await getIP.getProxy(ip, "api/v1/statics/");
+      setFilteredItems(response.data);
     }
-    
 
-    const filter = (e) => {
-        const keyword = e.target.value;
+    fetchData();
+  }, [ip]);
 
-        if (keyword !== '') {
-            const results = value.filter((image) => {
-                return image.toLowerCase().indexOf(keyword.toLowerCase()) !== -1;
-            });
-            setFilteredItems(results);
-        } else {
-            setFilteredItems(value);
-        }
+  const getIconForFile = (item) => {
+    const re = /(?:\.([^.]+))?$/;
+    const extension = re.exec(item)[1];
+    switch (extension) {
+      case "png":
+      case "jpg":
+      case "jpeg":
+        return (
+          <img
+            className={classes.image}
+            src={`/proxy/${ip}/api/v1/download/${item}`}
+          />
+        );
 
-        setImage(keyword);
-    };
+      default:
+        return "";
+    }
+  };
 
-    return (
+  return (
+    <>
+      {filteredItems && filteredItems.length > 0 ? (
         <>
-            {value &&
-                <>
-                    <Grid container justifyContent="flex-start">
-                        <TextField
-                            variant="outlined"
-                            placeholder='Search'
-                            type="search"
-                            value={image}
-                            onChange={filter}
-                            className={classes.search}
-                        />
-                    </Grid>
-                    <Grid container justifyContent="center">
-                        {filteredItems && filteredItems.length > 0 ?
-                            (filteredItems.map((item => (
-                                <Grid item xs={12} md={6} lg={4} key={item}>
-                                    <div>{getIconForFile(item)}</div>
-                                    <Link to={`/proxy/${ip}/api/v1/download/${item}`} target="_blank" ><DownloadIcon /> {item}</Link>
-                                </Grid>
-                            )))
-                            ) : (
-                                <Typography>No results found!</Typography>
-                            )
-                        }
-                    </Grid>
-                </>
-            }
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Thumbnail</TableCell>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Creation Date</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {filteredItems.map((item) => (
+                  <TableRow key={item[1]}>
+                    <TableCell>{getIconForFile(item[1])}</TableCell>
+                    <TableCell>
+                      <Link
+                        to={`/proxy/${ip}/api/v1/download/${item[1]}`}
+                        target="_blank"
+                      >
+                        {" "}
+                        {item[1]}
+                      </Link>
+                    </TableCell>
+                    <TableCell>
+                      {moment.unix(item[2]).format("MMM DD HH:mm")}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </>
-    )
-}
+      ) : (
+        <h3>No static resources</h3>
+      )}
+    </>
+  );
+};
 
 export default StaticsScreen;
